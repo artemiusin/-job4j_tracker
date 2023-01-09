029234459fea372403045fbb5fd064a0858097be
artemYsin <ysin.artem@gmail.com>
2023-01-09 17:15:14 +0300

    1.5. Поля объекта [#166083]

--- src/test/java/ru/job4j/oop/Cat.java
+++ src/test/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String eat) {
+        this.food = eat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        System.out.println("There are gav's food.");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        System.out.println("There are black's food.");
+        black.eat("fish");
+        black.show();
     }
 }
