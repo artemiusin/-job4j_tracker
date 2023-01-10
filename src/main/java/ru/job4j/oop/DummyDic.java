16b10f39144a3da61e1142412e1a01b1e8d18673
artemYsin <ysin.artem@gmail.com>
2023-01-10 09:37:44 +0300

    исправление кодировки

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,6 +2,6 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        return "Неизвестное слово. " + eng;
+        return "Неизвестное слово." + eng;
     }
 }
