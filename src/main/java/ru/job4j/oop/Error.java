7ff79943ff9e4e7f0c58f602b54dd89f6766afe3
artemYsin <ysin.artem@gmail.com>
2023-01-10 10:51:50 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(this.active);
+        System.out.println(this.status);
+        System.out.println(this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 1, "Hi");
+        Error error2 = new Error(false, 0, "Bye");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
