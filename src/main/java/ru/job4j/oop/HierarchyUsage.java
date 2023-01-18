790c93163d920742fa17f8af772e9426ce6c9b20
artemYsin <ysin.artem@gmail.com>
2023-01-18 13:12:03 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

--- src/main/java/ru/job4j/oop/HierarchyUsage.java
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -2,19 +2,12 @@ package ru.job4j.oop;
 
 public class HierarchyUsage {
     public static void main(String[] args) {
-        /* создаем объекта класса Car. */
         Car car = new Car();
-        /* делаем приведение к типу родителя Transport. */
         Transport tp = car;
-        /* делаем приведение к типу родителя Object. */
         Object obj = car;
-        /* Приведение типа при создании объекта. */
         Object ocar = new Car();
-        /* Приведение типа за счет понижения по иерархии. */
         Car carFromObject = (Car) ocar;
-
-        /* Ошибка в приведении типа. */
         Object bicycle = new Bicycle();
-        Car cb = (Car) bicycle; // код завершится с ошибкой приведения типов ClassCastException
+        Car cb = (Car) bicycle;
     }
 }
