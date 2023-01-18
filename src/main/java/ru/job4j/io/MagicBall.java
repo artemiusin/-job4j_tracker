0f37ccbbdce0232324e5a3519c258c3a6af2a5f4
artemYsin <ysin.artem@gmail.com>
2023-01-18 11:00:25 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514 #381795]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -10,9 +10,9 @@ public class MagicBall {
         String question = scanner.nextLine();
         int answer = new Random().nextInt(3);
         switch (answer) {
-            case 0 -> System.out.println("Да");
-            case 1 -> System.out.println("Нет");
-            default -> System.out.println("Может быть");
+            case 0 -> System.out.println("Yes");
+            case 1 -> System.out.println("No");
+            default -> System.out.println("May be");
         }
     }
 }
