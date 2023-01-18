2fed221e647078bca7ff9da943cb23888e4a454f
artemYsin <ysin.artem@gmail.com>
2023-01-18 12:46:30 +0300

    4.2. Интерфейс Input [#181092 #382443]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,6 +1,6 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport{
+public class Bus implements Transport {
     @Override
     public void drive() {
         System.out.println("Drive");
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -2,6 +2,8 @@ package ru.job4j.poly;
 
 public interface Transport {
     void drive();
+
     void passengers(int passengers);
+
     int refuel(int quantity);
 }
