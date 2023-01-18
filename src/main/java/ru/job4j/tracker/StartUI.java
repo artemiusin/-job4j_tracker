cc1ca673f4bb50881ea2a114faa53a2024455891
artemYsin <ysin.artem@gmail.com>
2023-01-18 10:06:17 +0300

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -54,6 +54,16 @@ public class StartUI {
                 } else {
                     System.out.println("Request deletion error.");
                 }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Item with id: " + id + " not found.");
+                }
             }
         }
     }
