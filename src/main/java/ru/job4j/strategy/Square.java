734138946dae171cb20ba3839c4bb36d0a71931f
artemYsin <ysin.artem@mail.ru>
2023-01-20 13:54:11 +0300

    11.0. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,11 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return " _____ " + ln
+                + "|     |" + ln
+                + "|_____|" + ln;
+    }
+}
