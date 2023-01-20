a59b6db66acaef291072f8e3bd96197eff750f77
artemYsin <ysin.artem@gmail.com>
2023-01-20 17:37:36 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780 #383202]

--- src/main/java/ru/job4j/tracker/ShowAction.java
+++ src/main/java/ru/job4j/tracker/ShowAction.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class ShowAction implements UserAction{
+public class ShowAction implements UserAction {
     @Override
     public String name() {
         return "Show all items";
