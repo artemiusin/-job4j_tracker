26782aff018b613c879eb26f5801e2f0e9d8de46
artemYsin <ysin.artem@gmail.com>
2023-01-30 16:06:19 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException("Element not found");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array = new String[]{"one","two","three"};
+        try {
+            indexOf(array, "five");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
