e8111e9fb7450241b22f5c840f479a5a4a211bea
artemYsin <ysin.atrem@gmail.com>
2023-01-31 15:31:19 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,7 +6,6 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
-            } else {
                 break;
             }
         }
