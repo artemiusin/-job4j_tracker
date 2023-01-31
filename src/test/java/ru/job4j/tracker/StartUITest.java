fb3dcd18062192c64361bea5ec547fcd15808aa0
artemYsin <ysin.artem@gmail.com>
2023-01-31 17:05:23 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -183,7 +183,7 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"7","0"}
+                new String[]{"7", "0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = new UserAction[]{
